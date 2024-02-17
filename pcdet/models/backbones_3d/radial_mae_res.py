222954828258890a36d28161fa8498d0499b87e8
sinatayebati <sinatayebati@gmail.com>
2024-02-17 17:21:04 -0600

    #13 radial_mae_res updates

--- pcdet/models/backbones_3d/radial_mae_res.py
+++ pcdet/models/backbones_3d/radial_mae_res.py
@@ -8,6 +8,7 @@ from ...utils.spconv_utils import replace_feature, spconv
 from ...utils import common_utils
 from .spconv_backbone import post_act_block
 
+
 class SparseBasicBlock(spconv.SparseModule):
     expansion = 1
 
@@ -21,10 +22,10 @@ class SparseBasicBlock(spconv.SparseModule):
         self.conv2 = spconv.SubMConv3d(
             planes, planes, kernel_size=3, stride=1, padding=1, bias=False, indice_key=indice_key
         )
-        self.bn2 = norm_fn(planes) 
+        self.bn2 = norm_fn(planes)
         self.downsample = downsample
         self.stride = stride
-    
+
     def forward(self, x):
         identity = x.features
 
@@ -44,3 +45,89 @@ class SparseBasicBlock(spconv.SparseModule):
         out = replace_feature(out, self.relu(out.features))
 
         return out
+    
+
+class Radial_MAE_res(nn.Module):
+    def __init__(self, model_cfg, input_channels, grid_size, **kwargs):
+        super().__init__()
+        self.model_cfg = model_cfg
+        norm_fn = partial(nn.BatchNorm1d, eps=1e-3, momentum=0.01)
+        
+        self.masked_ratio = model_cfg.MASKED_RATIO
+        self.angular_range = model_cfg.get('ANGULAR_RANGE', 5)  # Default to 5 if not specified
+        
+        self.sparse_shape = grid_size[::-1] + [1, 0, 0]
+
+        self.conv_input = spconv.SparseSequential(
+            spconv.SubMConv3d(input_channels, 16, 3, padding=1, bias=False, indice_key='subm1'),
+            norm_fn(16),
+            nn.ReLU(),
+        )
+        block = post_act_block
+
+        self.conv1 = spconv.SparseSequential(
+            SparseBasicBlock(16, 16, norm_fn=norm_fn, indice_key='res1'),
+            SparseBasicBlock(16, 16, norm_fn=norm_fn, indice_key='res1'),
+        )
+
+        self.conv2 = spconv.SparseSequential(
+            # [1600, 1408, 41] <- [800, 704, 21]
+            block(16, 32, 3, norm_fn=norm_fn, stride=2, padding=1, indice_key='spconv2', conv_type='spconv'),
+            SparseBasicBlock(32, 32, norm_fn=norm_fn, indice_key='res2'),
+            SparseBasicBlock(32, 32, norm_fn=norm_fn, indice_key='res2'),
+        )
+
+        self.conv3 = spconv.SparseSequential(
+            # [800, 704, 21] <- [400, 352, 11]
+            block(32, 64, 3, norm_fn=norm_fn, stride=2, padding=1, indice_key='spconv3', conv_type='spconv'),
+            SparseBasicBlock(64, 64, norm_fn=norm_fn, indice_key='res3'),
+            SparseBasicBlock(64, 64, norm_fn=norm_fn, indice_key='res3'),
+        )
+
+        self.conv4 = spconv.SparseSequential(
+            # [400, 352, 11] <- [200, 176, 5]
+            block(64, 128, 3, norm_fn=norm_fn, stride=2, padding=(0, 1, 1), indice_key='spconv4', conv_type='spconv'),
+            SparseBasicBlock(128, 128, norm_fn=norm_fn, indice_key='res4'),
+            SparseBasicBlock(128, 128, norm_fn=norm_fn, indice_key='res4'),
+        )
+
+        last_pad = 0
+        last_pad = self.model_cfg.get('last_pad', last_pad)
+        self.conv_out = spconv.SparseSequential(
+            # [200, 150, 5] -> [200, 150, 2]
+            spconv.SparseConv3d(128, 128, (3, 1, 1), stride=(2, 1, 1), padding=last_pad,
+                                bias=False, indice_key='spconv_down2'),
+            norm_fn(128),
+            nn.ReLU(),
+        )
+        
+        self.num_point_features = 16                
+
+        self.deconv1 = nn.Sequential(
+            nn.ConvTranspose3d(128, 32, 3, padding=1, output_padding=1, stride=2, bias=False),
+            nn.BatchNorm3d(32),
+            nn.ReLU()
+        )
+        self.deconv2 = nn.Sequential(
+            nn.ConvTranspose3d(32, 8, 3, padding=1, output_padding=1, stride=(4,2,2), bias=False),
+            nn.BatchNorm3d(8),
+            nn.ReLU()
+        )
+        self.deconv3 = nn.Sequential(
+            nn.ConvTranspose3d(8, 1, 3, padding=1, output_padding=1, stride=(3,2,2), bias=False),
+        )
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.forward_re_dict = {}
+        
+    def get_loss(self, tb_dict=None):
+        tb_dict = {} if tb_dict is None else tb_dict
+        pred = self.forward_re_dict['pred']
+        target = self.forward_re_dict['target']
+        loss = self.criterion(pred, target)
+
+        tb_dict = {
+            'loss_rpn': loss.item()
+        }
+
+        return loss, tb_dict
+    
